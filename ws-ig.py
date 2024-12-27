4239342f3a1b7edfe2ef47e56bba519d60fc09c4
JoshuaGlaZ <128287164+JoshuaGlaZ@users.noreply.github.com>
2024-12-27 22:23:50 +0700

    ig change firefox to chrome

--- ws-ig.py
+++ ws-ig.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
@@ -12,29 +12,21 @@ import sys
 import json
 from fake_useragent import UserAgent
 
-# Configure Firefox options
+# Configure Chrome options
 options = Options()
-ua = UserAgent()  # Random user agent
-# options.add_argument("-headless")  # Run in headless mode
-options.set_preference("general.useragent.override",
-                       ua.random)  # Set a random user agent
-options.set_preference("dom.webdriver.enabled",
-                       False)  # Disable webdriver flag
-options.set_preference("dom.webnotifications.enabled",
-                       False)  # Disable notifications
-# # Enable tracking protection
-options.set_preference("privacy.trackingprotection.enabled", True)
-# # Disable images for faster loading (1 = enabled, 2 = disabled)
-options.set_preference("permissions.default.image", 2)
-# options.set_preference("permissions.default.stylesheet", 2)  # Disable CSS
-# # Disable WebRTC to prevent IP leakage
-options.set_preference("media.peerconnection.enabled", False)
-# # Disable media devices to avoid detection
-options.set_preference("media.navigator.enabled", False)
+ua = UserAgent()
+options.add_argument(f'user-agent={ua.random}')
+options.add_argument('--disable-notifications')
+options.add_argument('--disable-images')
+options.add_argument('--disable-web-security')
+options.add_argument('--disable-blink-features=AutomationControlled')
+options.add_argument('--disable-webrtc')
+options.add_argument('--disable-media-stream')
+options.add_argument('--enable-tracking-protection')
 
 cookie = "instagram_cookies.pkl"
 
-keywords = sys.argv[1]  # ws-ig.py "piala dunia 2000"
+keywords = sys.argv[1]
 
 posts_output = []
 
@@ -43,17 +35,14 @@ max_comment = 3
 max_reply = 3
 post_num = 0
 
-
 def save_cookies(driver, file_path):
     with open(file_path, "wb") as file:
         pickle.dump(driver.get_cookies(), file)
-
 
 def load_cookies(driver, file_path):
     with open(file_path, "rb") as file:
         for cookie in pickle.load(file):
             driver.add_cookie(cookie)
-
 
 def login_instagram(driver):
     driver.get("https://www.instagram.com")
@@ -63,10 +52,8 @@ def login_instagram(driver):
     password = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.NAME, "password")))
     username.clear()
-    # username.send_keys("ws.agent0001")
     username.send_keys("ttusj_7_.1")
     password.clear()
-    # password.send_keys("dummypassword")
     password.send_keys("dummypassword")
     login_button = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']")))
@@ -75,7 +62,6 @@ def login_instagram(driver):
     WebDriverWait(driver, 10).until(EC.presence_of_element_located(
         (By.CSS_SELECTOR, "svg[aria-label='Beranda']")))
     save_cookies(driver, cookie)
-
 
 def safe_click(driver, element):
     retries = 3
@@ -86,7 +72,6 @@ def safe_click(driver, element):
         except StaleElementReferenceException:
             time.sleep(0.1)
     raise TimeoutException("Failed to click the element after retries")
-
 
 def scrape_instagram(driver, keywords):
     keywords = '+'.join(keywords.split(' '))
@@ -99,7 +84,6 @@ def scrape_instagram(driver, keywords):
         result_links = []
 
         try:
-            # Retrieve result links
             results = WebDriverWait(driver, 5).until(
                 EC.presence_of_all_elements_located((By.CSS_SELECTOR, "a h3"))
             )
@@ -117,19 +101,17 @@ def scrape_instagram(driver, keywords):
             WebDriverWait(driver, 10).until(
                 lambda d: d.current_url != "about:blank")
 
-            # "https://www.instagram.com/officialinewstv/"
             if len(driver.current_url.split('/')) == 5:
                 driver.close()
                 driver.switch_to.window(driver.window_handles[0])
                 continue
 
-            # "https://www.instagram.com/p/C93AkvGBlci/?hl=af"
             elif len(driver.current_url.split('/')) == 6:
                 user_post = WebDriverWait(driver, 5).until(EC.presence_of_element_located(
                     (By.CSS_SELECTOR, "span._ap3a._aaco._aacw._aacx._aad7._aade"))).text
 
                 current_url = driver.current_url.split('/')
-                if user_post:  # Check if user_post is not empty
+                if user_post:
                     current_url.insert(3, user_post)
                     current_url = '/'.join(current_url)
                     driver.get(current_url)
@@ -145,8 +127,6 @@ def scrape_instagram(driver, keywords):
                 post_caption = "No caption available for this post."
 
             post_count += 1
-            # print(f"Post {post_count}: {post_caption}")
-            # print(f"Link: {driver.current_url}")
             text = post_caption
 
             try:
@@ -166,7 +146,6 @@ def scrape_instagram(driver, keywords):
                     comment_text = comment.find_element(
                         By.CSS_SELECTOR, 'div._a9zs span._ap3a._aaco._aacu._aacx._aad7._aade').text
                 except NoSuchElementException:
-                    # If there’s no text, it could be an image-only comment
                     try:
                         image_element = comment.find_element(
                             By.CSS_SELECTOR, 'img')
@@ -175,10 +154,8 @@ def scrape_instagram(driver, keywords):
                     except NoSuchElementException:
                         comment_text = "No text or image in comment"
 
-                # print(f"User {comment_num+1} by {user_comment}: {comment_text}")
                 text += f"\n{user_comment}: {comment_text}" 
 
-                # Click more replies
                 click_reply = 0
                 while click_reply < max_reply:
                     try:
@@ -190,7 +167,6 @@ def scrape_instagram(driver, keywords):
                     except (NoSuchElementException, TimeoutException, StaleElementReferenceException):
                         break
 
-                # Load replies if available
                 try:
                     comment_replies = comment.find_element(
                         By.CSS_SELECTOR, 'ul._a9yo')
@@ -202,7 +178,6 @@ def scrape_instagram(driver, keywords):
                         reply_text = reply.find_element(
                             By.CSS_SELECTOR, 'div._a9zs span').text
 
-                        # print(f"\tReply {reply_num + 1} by {user_reply}: {reply_text}")
                         text += f"\n\t{user_reply}: {reply_text}" 
 
                 except (NoSuchElementException, TimeoutException, StaleElementReferenceException):
@@ -213,7 +188,6 @@ def scrape_instagram(driver, keywords):
             driver.close()
             driver.switch_to.window(driver.window_handles[0])
 
-        # Attempt to load next page of results
         if post_count < max_post:
             try:
                 next_button = WebDriverWait(driver, 5).until(
@@ -222,11 +196,9 @@ def scrape_instagram(driver, keywords):
                 )
                 next_button.click()
             except TimeoutException:
-                # print("No more pages available.")
                 break
 
-
-driver = webdriver.Firefox(options=options)
+driver = webdriver.Chrome(options=options)
 
 try:
     if os.path.exists(cookie):
